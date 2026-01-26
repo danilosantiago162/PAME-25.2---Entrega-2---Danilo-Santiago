8a176a58d4ef45391a9dfbc0f5b14d8bb20296f0
Danilo <danilo.santiago162@gmail.com>
2026-01-26 13:40:41 -0300

    Implementação do método Login e funções auxiliares

--- Sistemas_Multas.js
+++ Sistemas_Multas.js
@@ -20,6 +20,38 @@ function gerarNumMatricula() {
 
 }
 
+function carregarCondutores() {
+
+    const condutores = fs.readFileSync("condutores.json", "utf-8");
+    return JSON.parse(condutores);
+
+}
+
+function carregarAgentes() {
+
+    const agentes = fs.readFileSync("agentes.json", "utf-8");
+    return JSON.parse(agentes);
+
+}
+
+function PaginaInicial(){
+    var opcao = requisicao.question("Bem vindo ao sistema de controle da Segue o Fluxo: " + "\n \n" + 
+    "O que deseja fazer? \n \n" + " Cadastro: Aperte 1 \n login: Aperte 2 \n Sair do Sistema: Aperte 3 \n"
+    );
+
+    if (opcao == "1"){
+        sistema.cadastro();
+    }
+
+    if (opcao == "2"){
+        sistema.login();
+    }
+
+    if (opcao == "3"){
+        //sistema.sair();
+    }
+}
+
 class Pessoa {
     constructor(id_unico, nome, cpf, email, senha){
         this.id_unico = id_unico;
@@ -79,20 +111,20 @@ class Sistema{
         let email = requisicao.question("Insira seu email: ");
         let senha = requisicao.question("Insira sua senha: ");
 
-        let opcao = requisicao.question("Caso queira cadastra um novo condutor aperte 1, caso queira cadastrar um novo agente aperte 2");
+        let opcao = requisicao.question("Caso queira se cadastrar como um novo condutor aperte 1, caso queira se cadastrar como um novo agente aperte 2 \n");
 
         if (opcao == "1"){
             let data_de_nascimento = requisicao.question("Qual sua data de nascimento? ");
 
-            let usuario;
+            let condutor;
 
-            usuario = new Condutor(id_unico, nome, cpf, email, senha, data_de_nascimento);
+            condutor = new Condutor(id_unico, nome, cpf, email, senha, data_de_nascimento);
 
-            this.salvar_usuario(usuario);
+            this.salvar_condutor(condutor);
 
             console.log("Cadastro realizado com sucesso!!");
 
-            console.log(usuario);
+            console.log(condutor);
         }
 
         if (opcao == "2"){
@@ -111,16 +143,53 @@ class Sistema{
         }
     }
 
-    salvar_usuario(usuario_add){
-        let usuarios_lista = [];
+    login(){
 
-        if (fs.existsSync("usuarios.json")){
-            usuarios_lista = JSON.parse(fs.readFileSync("usuarios.json", "utf-8"));
+        let opcao = requisicao.question("Se voce for um condutor cadastrado, aperte 1. \nSe voce for um agente cadastrado, aperte 2? \n")
+
+        let email = requisicao.question("Insira seu email: ");
+        let senha = requisicao.question("Insira sua senha: ");
+
+
+        switch(opcao){
+            case "1":
+
+                let condutores = carregarCondutores();
+                let condutor_encontrado = condutores.find(condutor => condutor.email === email && condutor.senha === senha);
+
+                if (!condutor_encontrado){
+                    console.log("Email ou senha incorreto");
+                } else {
+
+                    console.log("Login realizado com sucesso!");
+                    //condutor_logado();
+                }
+                break;
+            case "2":
+
+                let agentes = carregarAgentes();
+                let agente_encontrado = agentes.find(agente => agente.email === email && agente.senha === senha);
+
+                if (!agente_encontrado){
+                    console.log("Email ou senha incorreto");
+                } else {
+
+                    console.log("Login realizado com sucesso!");
+                    //agente_logado();
+                }
+        }
+    }
+
+    salvar_condutor(condutor_add){
+        let condutores_lista = [];
+
+        if (fs.existsSync("condutores.json")){
+            condutores_lista = JSON.parse(fs.readFileSync("condutores.json", "utf-8"));
         }
 
-        usuarios_lista.push(usuario_add);
+        condutores_lista.push(condutor_add);
 
-        fs.writeFileSync("usuarios.json", JSON.stringify(usuarios_lista, null, 2));
+        fs.writeFileSync("condutores.json", JSON.stringify(condutores_lista, null, 2));
     }
 
     salvar_agente(agente_add){
@@ -140,19 +209,5 @@ class Sistema{
 
 var sistema = new Sistema();
 
-var opcao = requisicao.question("Bem vindo ao sistema de controle da Segue o Fluxo: " + "\n \n" + 
-    "O que deseja fazer? \n \n" + " Cadastro: Aperte 1 \n login: Aperte 2 \n Sair do Sistema: Aperte 3"
-);
-
-if (opcao == "1"){
-    sistema.cadastro();
-}
-
-if (opcao == "2"){
-    //sistema.login();
-}
-
-if (opcao == "3"){
-    //sistema.sair();
-}
+PaginaInicial();
 
